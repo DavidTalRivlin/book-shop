e0016e4850ba29b0aad7da75ef430fabab6e7811
DavidTalRivlin <david.dave.tal@gmail.com>
2023-11-19 16:18:34 +0200

    Add onInit and books table

---
+++ controller.js
@@ -0,0 +1,39 @@
+'use strict'
+
+function onInit() {
+      renderBooks()
+}
+
+function renderBooks() {
+
+      const elTable = document.querySelector('.books-table')
+      const keys = Object.keys(gBooks[0])
+      var strHtml = `
+      <thead>
+         <tr>`
+
+      strHtml += keys.map((key) => `<th>${key}</th>`).join('')
+
+      strHtml += `
+         </tr>
+      </thead>
+      <tbody>`
+
+      strHtml += gBooks.map((book) => {
+            return `<tr>
+      <td>${book.id}</td>
+      <td>${book.name}</td>
+      <td>${book.price}</td>
+      <td><img src="${book.imgUrl}" alt="${book.name}"></td>
+      </tr>`
+      }).join('')
+
+      strHtml += `</tbody>
+            </table>`
+
+      elTable.innerHTML = strHtml
+
+
+
+
+}
---
+++ service/book-service.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var gBooks = [
+      {
+            id: 123,
+            name: 'itchy nose giraffe',
+            price: 74,
+            imgUrl: 'img/giraffe.jpg',
+
+      },
+      {
+            id: 456,
+            name: 'ayalet metayelet',
+            price: 50,
+            imgUrl: 'img/ayalet.jpg',
+
+      },
+      {
+            id: 789,
+            name: 'stroy of a knight',
+            price: 70,
+            imgUrl: 'img/abir.jpg',
+
+      }
+]
+
+
+
