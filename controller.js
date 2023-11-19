64cff2632738a523697176ac174c1501ef43a88a
DavidTalRivlin <david.dave.tal@gmail.com>
2023-11-19 19:31:36 +0200

    Modal & Add/Remove book added

--- controller.js
+++ controller.js
@@ -7,30 +7,29 @@ function onInit() {
 function renderBooks() {
 
       const elTable = document.querySelector('.books-table')
-      const keys = Object.keys(gBooks[0])
+      // const keys = Object.keys(gBooks[0])
       var strHtml = `
       <thead>
-         <tr>`
-
-      strHtml += keys.map((key) => `<th>${key}</th>`).join('')
-
-      strHtml += `<th>Actions</th>`
-
-      strHtml += `
+         <tr>
+         <td>PN</td>
+         <td>Name</td>
+         <td>Price</td>
+         <td>Image</td>
+         <td>Actions</td>
          </tr>
-      </thead>
-      <tbody>`
+         </thead>
+         <tbody>`
 
       strHtml += gBooks.map((book) => {
             return `<tr>
       <td>${book.id}</td>
       <td>${book.name}</td>
       <td>${book.price}</td>
-      <td><img src="${book.imgUrl}" alt="${book.name}"></td>
+      <td><img src="${book.imgUrl}" alt="${book.name}" class="book-img"></td>
       <td> 
-      <button class="book-btn read-book-btn" onclick="onRead(${book.id})">Read</button>
-      <button class="book-btn update-book-btn" onclick="onUpdate(${book.id})">Update</button>
-      <button class="book-btn delete-book-btn" onclick="onDelete(${book.id})">Delete</button>
+      <button class="book-btn read-book-btn" onclick="onReadBook('${book.id}')">Read</button>
+      <button class="book-btn update-book-btn" onclick="onUpdateBook('${book.id}')">Update</button>
+      <button class="book-btn delete-book-btn" onclick="onDeleteBook('${book.id}')">Delete</button>
       </td>
       </tr>`
       }).join('')
@@ -48,6 +47,32 @@ function renderBooks() {
 function onAddBook() {
       var name = prompt('please enter book name')
       var price = prompt('please enter price')
-      addBook(name,price)
+      addBook(name, price)
       renderBooks()
 }
+
+
+function onDeleteBook(bookId) {
+      removeBook(bookId)
+      flashMsg(`Booked Removed`)
+}
+
+
+function onUpdateBook(bookId) {
+      var newBookPrice = +prompt('please type new price...')
+      updateBook(bookId, newBookPrice)
+      flashMsg(`Book's price updated`)
+}
+
+
+function onReadBook(bookId) {
+
+}
+
+function flashMsg(msg) {
+      const elUserMsg = document.querySelector('.user-msg')
+  
+      elUserMsg.innerText = msg
+      elUserMsg.classList.add('open')
+      setTimeout(() => elUserMsg.classList.remove('open'), 3000)
+  }
--- service/book-service.js
+++ service/book-service.js
@@ -9,80 +9,82 @@ function saveBooks() {
 }
 
 
-
-function addBook(name,price) {
-      const book = _createBook(name,price)
+function addBook(name, price) {
+      const book = _createBook(name, price)
       gBooks.unshift(book)
-  
+
       _saveBooksToStorage()
-  }
-
-// function removeBook(carId) {
-//       const carIdx = gCars.findIndex(car => carId === car.id)
-//       gCars.splice(carIdx, 1)
-      
-//       _saveCarsToStorage()
-//   }
-  
-
-
-//   function updateCar(carId, newSpeed) {
-//       const car = gCars.find(car => car.id === carId)
-//       car.maxSpeed = newSpeed
-  
-//       _saveCarsToStorage()
-//       return car
-
-
-
-//////////// private functions
-
-function _createBook(name, price){
-      return {
-            id: makeId(),
-            name,
-            price,
-            imgUrl: `img/${getRandomInt(1,6)}.jpg`,
-      }
+      renderBooks()
 }
 
 
-function _createBooks() {
+function removeBook(BookId) {
+      const bookIdx = gBooks.findIndex(book => BookId === book.id)
+      gBooks.splice(bookIdx, 1)
 
-
-      var books = loadFromStorage('booksDB')
-      if (!books || !books.length) {
-
-
-            books = [
-                  {
-                        id: makeId(),
-                        name: 'itchy nose giraffe',
-                        price: 74,
-                        imgUrl: 'img/giraffe.jpg',
-
-                  },
-                  {
-                        id: makeId(),
-                        name: 'ayalet metayelet',
-                        price: 50,
-                        imgUrl: 'img/ayalet.jpg',
-
-                  },
-                  {
-                        id: makeId(),
-                        name: 'stroy of a knight',
-                        price: 70,
-                        imgUrl: 'img/abir.jpg',
-
-                  }]
-                  saveToStorage('booksDB',books)
-                  
-      }
-      gBooks = books
+      _saveBooksToStorage()
+      renderBooks()
 }
 
 
-function _saveBooksToStorage() {
-      saveToStorage(STORAGE_KEY, gBooks)
-  }
+
+function updateBook(bookId, newPrice) {
+      const book = gBooks.find(book => book.id === bookId)
+      book.price = newPrice
+
+      _saveBooksToStorage()
+      renderBooks()
+}
+
+
+      //////////// private functions
+
+      function _createBook(name, price) {
+            return {
+                  id: makeId(),
+                  name,
+                  price,
+                  imgUrl: `img/${getRandomInt(1, 6)}.jpg`,
+            }
+      }
+
+
+      function _createBooks() {
+
+
+            var books = loadFromStorage('booksDB')
+            if (!books || !books.length) {
+
+
+                  books = [
+                        {
+                              id: makeId(),
+                              name: 'itchy nose giraffe',
+                              price: 74,
+                              imgUrl: 'img/giraffe.jpg',
+
+                        },
+                        {
+                              id: makeId(),
+                              name: 'ayalet metayelet',
+                              price: 50,
+                              imgUrl: 'img/ayalet.jpg',
+
+                        },
+                        {
+                              id: makeId(),
+                              name: 'stroy of a knight',
+                              price: 70,
+                              imgUrl: 'img/abir.jpg',
+
+                        }]
+                  saveToStorage('booksDB', books)
+
+            }
+            gBooks = books
+      }
+
+
+      function _saveBooksToStorage() {
+            saveToStorage(STORAGE_KEY, gBooks)
+      }
